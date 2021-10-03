68e9fab017da1e78f330cd1b30ef474bc8edca89
sarthak37 <sarthak27998@gmail.com>
2021-10-03 19:08:44 +0530

    p

---
+++ App.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import SplashScreen from './src/screens/SplashScreen';
+import InitialLaunchScreen from './src/screens/InitialLaunchScreen';
+import SignUpScreen1 from './src/screens/SignUpScreen1';
+import SignUpScreen2 from './src/screens/SignUpScreen2';
+import FeedScreen from './src/screens/FeedScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import SearchHomeScreen from './src/screens/SearchHomeScreen';
+import SearchScreen from './src/screens/SearchScreen';
+import GalleryScreen from './src/screens/GalleryScreen';
+import PhotoCaptureScreen from './src/screens/PhotoCaptureScreen';
+import ApplyEffectScreen from './src/screens/ApplyEffectScreen';
+import ActivityNotificationScreen from './src/screens/ActivityNotificationScreen';
+const App: () => React$Node = () => {
+  return (
+    <View style={styles.container}>
+      <ActivityNotificationScreen />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+  },
+});
+
+export default App;
